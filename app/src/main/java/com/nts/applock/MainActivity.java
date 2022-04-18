80a620b985d9230527ba798fcc7c1141913df847
XPS-9343\nts69 <nts6920@gmail.com>
2022-04-18 21:17:12 +0700

    first commit

---
+++ app/src/main/java/com/nts/applock/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nts.applock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
